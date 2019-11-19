94b4e5d0d5c2af1ba70f9ce85dd9b5af5d011d07
TiepPQ <traitrang.pham91@gmail.com>
2019-11-19 08:54:39 +0700

    Update getBHYT.js

--- getBHYT.js
+++ getBHYT.js
@@ -416,21 +416,29 @@ function Check_Get_BHYT(ReSult_BHYT)
 									bool_Gioitinh = 'false';
                                     Value_Gioitinh = 0;
 								}
-								/*var DauThe = ReSult_BHYT.maThe.substring(0,3);
+								var DauThe = ReSult_BHYT.maThe.substring(0,3);
 								var url1 = "https://yte-hungyen.vnpthis.vn/web_his/kiemtrathebhyt?madt="+DauThe;
-								$.ajax({
+								/*$.ajax({
 								url : url1,
 								type : "post",
 								contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
 								dataType:"json",
 								success : function (result){
 								var res = result.split(":");*/
-									if(ReSult_BHYT.ghiChu.indexOf('Chủ thẻ đã được cấp mã thẻ mới')>-1 ||ReSult_BHYT.ghiChu.indexOf('Thẻ được gia hạn thêm')>-1)
-										SetValueThongTinBHYT(ReSult_BHYT.maTheMoi,ReSult_BHYT.hoTen,ReSult_BHYT.ngaySinh,bool_namsinh,ReSult_BHYT.gtTheTuMoi,ReSult_BHYT.gtTheDenMoi,ReSult_BHYT.maDKBDMoi,ReSult_BHYT.maDKBDMoi,bool_Gioitinh,Value_Gioitinh,true,ReSult_BHYT.diaChi,"");
-									else
-										SetValueThongTinBHYT(ReSult_BHYT.maThe,ReSult_BHYT.hoTen,ReSult_BHYT.ngaySinh,bool_namsinh,ReSult_BHYT.gtTheTu,ReSult_BHYT.gtTheDen,ReSult_BHYT.maDKBD,ReSult_BHYT.maDKBD,bool_Gioitinh,Value_Gioitinh,true,ReSult_BHYT.diaChi,"");
-									/*}
-								});*/
+								var xhttp = new XMLHttpRequest();
+								xhttp.onreadystatechange = function() {
+									if (this.readyState == 4 && this.status == 200) {
+											var result = this.responseText;
+											var res = result.split(":");
+											if(ReSult_BHYT.ghiChu.indexOf('Chủ thẻ đã được cấp mã thẻ mới')>-1 ||ReSult_BHYT.ghiChu.indexOf('Thẻ được gia hạn thêm')>-1)
+												SetValueThongTinBHYT(ReSult_BHYT.maTheMoi,ReSult_BHYT.hoTen,ReSult_BHYT.ngaySinh,bool_namsinh,ReSult_BHYT.gtTheTuMoi,ReSult_BHYT.gtTheDenMoi,ReSult_BHYT.maDKBDMoi,ReSult_BHYT.maDKBDMoi,bool_Gioitinh,Value_Gioitinh,true,ReSult_BHYT.diaChi,res[1]);
+											else
+												SetValueThongTinBHYT(ReSult_BHYT.maThe,ReSult_BHYT.hoTen,ReSult_BHYT.ngaySinh,bool_namsinh,ReSult_BHYT.gtTheTu,ReSult_BHYT.gtTheDen,ReSult_BHYT.maDKBD,ReSult_BHYT.maDKBD,bool_Gioitinh,Value_Gioitinh,true,ReSult_BHYT.diaChi,res[1]);
+									 }
+								};
+								xhttp.open("POST", DauThe, true);
+								xhttp.setRequestHeader("Content-type", "application/json");
+								xhttp.send("Your JSON Data Here");
 								
 							}
 	else{
